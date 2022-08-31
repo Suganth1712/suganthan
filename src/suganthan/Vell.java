549a30e81becbcfbd98009cda88e4b0c56494bd3
elcot <elcot@boss>
2022-08-31 10:45:55 +0530

    java swapping

--- src/suganthan/Vell.java
+++ src/suganthan/Vell.java
@@ -1,11 +1,23 @@
 package suganthan;
 
-public class Vell {
-
-	public static void main(String[] args) {
-		System.out.println("mani");
-		// TODO Auto-generated method stub
-
-	}
-
+public class Vell
+{
+	public static void main(String args[])
+	{
+	int x=-6;
+	int y=8;
+	int z=-9;
+	x=x+y+z;
+	z=x-y-z;
+	y=x-y-z;
+	x=x-y-z;
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(z);
+	
+	
+	
+	int age=10;
+	System.out.println(age>=18);
+	}	
 }
