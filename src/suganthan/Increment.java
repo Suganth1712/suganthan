40bfe6cce2831a0e9a37c965da8ef2442afd6628
elcot <suganthan>
2022-09-05 11:21:05 +0530

    Java increment

---
+++ src/suganthan/Increment.java
@@ -0,0 +1,19 @@
+package suganthan;
+
+public class Increment {
+
+public static void main(String args[])
+{
+	int a=3,b=5;
+	if(a>2 && a++>2)
+	{
+		++b;
+	}
+	else
+	{
+		a++;
+	}
+	System.out.println(a);
+	System.out.println(b);
+}
+}
--- src/suganthan/Vell.java
+++ src/suganthan/Vell.java
@@ -4,20 +4,109 @@ public class Vell
 {
 	public static void main(String args[])
 	{
-	int x=-6;
-	int y=8;
-	int z=-9;
-	x=x+y+z;
-	z=x-y-z;
-	y=x-y-z;
-	x=x-y-z;
-	System.out.println(x);
-	System.out.println(y);
-	System.out.println(z);
+	int a=11;
+	if(a%2==0 && a>=0)
+	{
+		System.out.println("postive even");
 	
+	}
 	
+	else if((a%2==0) && (a<=0))
+	{
+		System.out.println("negative even");
+	}
+	else if(!(a%2==0) && (a>=0))
+	{
+		System.out.println("negative odd");
 	
-	int age=10;
-	System.out.println(age>=18);
-	}	
+	}
+	else if(!(a%2==0) && (a<=0))
+	{
+		System.out.println("postive odd ");
+	}
+}
+}char a;
+a='a';
+char aUpperCase=Character.toUpperCase(a);
+System.out.println(aUpperCase);
+}	package suganthan;
+
+public class Vell
+{
+public static void main(String args[])
+{
+int x=-6;
+int y=8;
+int z=-9;
+x=x+y+z;
+z=x-y-z;
+y=x-y-z;
+x=x-y-z;
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+
+
+
+//int age=10;
+//System.out.println(age>=18);
+//executed condtion is true if used
+//int a=20;
+//int b=10;
+if (20<10);
+{
+	System.out.println("20 is greater  than 18");
+}
+int age=20;
+int height=172;
+int weight=70;
+if(height<180)
+{
+	System.out.println("height eligible");
+}
+else
+{
+	System.out.println("height not eligible");
+}
+if(age<10)
+{
+
+	System.out.println("eligible");
+}	
+else if(age<10)
+{
+  System.out.println("not eligible");
+		
+	}
+else
+{
+	System.out.println("your not eligible");
+}
+if(weight<50)
+{
+	System.out.println("eligible weight ");
+}
+else
+{
+	System.out.println("not eligibile weight");
+}
+//char a='a';
+//b=Character.toUpperCase(a);
+//System.out.println(a);
+//char a,b;
+char a='a';
+//b=Character.toUpperCase(a);
+System.out.println(a);
+int m=20;
+if(m%2==0)
+{
+	System.out.println("is even");
+}
+else {
+	System.out.println("is odd");
+}
+
+}	
+}
+
 }
