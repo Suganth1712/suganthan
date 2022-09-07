0f26878609f0768dba9d523d076941c846790030
elcot <elcot@boss>
2022-09-07 08:45:01 +0530

    check which is greater than a and b

---
+++ src/suganthan/Largestvalue.java
@@ -0,0 +1,23 @@
+package suganthan;
+
+public class Largestvalue {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=5;
+		if (a>b)
+		{
+			System.out.println("a is greater value");
+		}
+		else if (a<b)
+		{
+			System.out.println("b is greater value");	
+		}
+		else
+		{
+			System.out.println("a & b are equal");
+		}
+
+	}
+
+}
