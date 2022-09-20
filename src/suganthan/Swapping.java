7285db7c74ce06e1e00c2adc3860ac78b0273701
elcot <suganthan>
2022-09-20 21:26:43 +0530

    java swapping

---
+++ src/suganthan/Swapping.java
@@ -0,0 +1,14 @@
+package suganthan;
+
+public class Swapping {
+public static void main (String args[])
+{
+	int a=5;
+	int b=7;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println(a);
+	System.out.println(b);
+}
+}
