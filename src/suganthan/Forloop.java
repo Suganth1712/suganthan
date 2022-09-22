763e255d560ea4749f67569d3636165237718abe
elcot <elcot@boss>
2022-09-22 11:53:53 +0530

    forloop

---
+++ src/suganthan/Forloop.java
@@ -0,0 +1,15 @@
+package suganthan;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		int p =0;
+		for (int i=5;i<=20;i+=5)
+		{
+			p=p+i;
+			
+		}
+		System.out.println(p);
+	}
+
+}
