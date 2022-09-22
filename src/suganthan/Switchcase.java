bc5654be17ec84093e68d410959b02d8d81adab3
elcot <elcot@boss>
2022-09-22 11:51:02 +0530

    switchcase

---
+++ src/suganthan/Switchcase.java
@@ -0,0 +1,31 @@
+package suganthan;
+
+public class Switchcase {
+	public static void main (String args[])
+	{
+		int day=7;
+		switch(day)
+		{
+		case 1:
+		case 2:
+		case 3:	
+
+		case 4:	
+
+		case 5:
+		{
+			System.out.println("weekdays");
+		}
+		break;
+		case 6:
+		case 7:
+		{
+	       System.out.println("weekend");
+		}
+	
+		}
+	
+
+}
+}
+
