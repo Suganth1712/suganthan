2894691ef112318973d7a04075570c3c9c6124aa
`elcot <elcot@boss>
2022-09-22 09:32:50 +0530

    Addition in java

---
+++ src/suganthan/Adding.java
@@ -0,0 +1,33 @@
+package suganthan;
+
+public class Adding {
+	public static void main(String[] args) 
+	{
+		
+		int n1= -1;
+		int n2 = 1;
+		int n3=0;
+		int j;
+		int n=4;
+        for(int a=0;a<=9;a++)
+        {
+        	
+        	
+			n3 = n1 + n2;
+			System.out.println(n3+"");
+			n1= n2;
+			n2 =n3;
+			
+		}
+        for(int i=0;i<=n;i++)
+        {
+    		
+			for(j=0;j<=i;j++)
+		
+		{
+			System.out.print(n3);
+		}
+		System.out.println();
+	}
+}
+}
