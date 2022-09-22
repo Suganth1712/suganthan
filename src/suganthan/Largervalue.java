678f19913a0f324680b11f8ecff0b44755597cbe
elcot <elcot@boss>
2022-09-22 11:54:52 +0530

    largevalue

---
+++ src/suganthan/Largervalue.java
@@ -0,0 +1,10 @@
+package suganthan;
+
+public class Largervalue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/suganthan/Largestvalue.java
+++ src/suganthan/Largestvalue.java
@@ -3,20 +3,22 @@ package suganthan;
 public class Largestvalue {
 
 	public static void main(String[] args) {
-		int a=5;
-		int b=5;
-		if (a>b)
+		int a=10;
+		int b=9;
+		int c=8;	
+		if((a>b)&&(a>c))//a greater than b &a is greater than c
 		{
 			System.out.println("a is greater value");
 		}
-		else if (a<b)
+		else if (b>c)
 		{
 			System.out.println("b is greater value");	
 		}
-		else
+		else 
 		{
-			System.out.println("a & b are equal");
+			System.out.println("c is greater");
 		}
+		
 
 	}
 
