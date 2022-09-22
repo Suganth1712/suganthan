97fa5af65e929cbc84934684c8fd25f6db0bba64
elcot <elcot@boss>
2022-09-22 11:49:57 +0530

    Digit

---
+++ src/suganthan/Digit.java
@@ -0,0 +1,17 @@
+package suganthan;
+
+public class Digit {
+
+	public static void main(String[] args) {
+		int  i=1;
+		int j=1;
+		while (j<=100)
+		{ 
+			System.out.println(i);
+			i=i+j;
+			j++;
+		}
+		
+	}
+
+}
