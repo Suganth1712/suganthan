b29480921227d455d7a98301c4ab16894ec70fe9
elcot <elcot@boss>
2022-09-22 09:26:40 +0530

    java programs<

---
+++ src/suganthan/Split.java
@@ -0,0 +1,43 @@
+package suganthan;
+
+public class Split {
+	public static void main(String args[] )
+	{
+	int num=67896;
+	
+	if(num>0)
+	{
+		int a = num%10;
+		System.out.println(a);
+		num=num/10;
+	}
+	if (num>0)
+	{
+		int a = num%10;
+		System.out.println(a);
+		num=num/10;
+	}
+	if (num>0)
+	{
+		int a = num%10;
+		System.out.println(a);
+		num=num/10;
+	}
+	if (num>0)
+	{
+		int a = num%10;
+		System.out.println(a);
+		num=num/10;
+	}
+	if (num>0)
+	{
+		int a = num%10;
+		System.out.println(a);
+		num=num/10;
+	}
+	
+	
+	
+}
+}
+
