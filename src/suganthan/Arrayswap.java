2918dea47e69b1d39297211a9522bff6a4b4d260
elcot <elcot@boss>
2022-09-28 09:00:47 +0530

    Arrayswapping

--- src/suganthan/Arrayswap.java
+++ src/suganthan/Arrayswap.java
@@ -3,23 +3,11 @@ package suganthan;
 public class Arrayswap {
 	public static void main(String args[])
 	{
-		
+		for(int i=0;i<=2;i++)
+		{
+			System.out.println((int)Math.pow(11,i));
+		}
 
-	int a[]= {1,2,3,7,6};
-    int j=a[4];
-   
-    for(int i=a.length-1;i>0;i--)
-	
-	{
-		a[i]=a[i-1];
-		
-	}
-	
-	a[0]=j;
-	for(int i=0;i<=a.length-1;i++)
-	{
-		System.out.println(a[i]);
-	}
 	
 }
 }
