b924b342cea4635542446894db4b07e40415ad71
elcot <elcot@boss>
2022-10-06 09:45:56 +0530

    Fibonacci series2

---
+++ src/suganthan/fibonacci2.java
@@ -0,0 +1,20 @@
+package suganthan;
+
+public class fibonacci2 {
+
+	public static void main(String args[])
+	{
+		int n=1;
+		int n1=1;
+	
+		
+		for(int i=0;i<=7;i++)
+		{
+			System.out.println(n+n1);
+			n=n+n1;
+			n1=n-n1;
+			
+			
+}
+}
+}
